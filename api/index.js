b74506f4299ca208481e16a13f02ed5f991ba932
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-26 17:19:09 -0500

    Added getKey route to generate a key to access Kroger API

--- api/index.js
+++ api/index.js
@@ -2,4 +2,5 @@ const router = require("express").Router()
 
 router.use("/user", require("./users"))
 router.use("/cart_items", require("./items"))
+router.use("/kroger", require("./kroger"))
 module.exports = router
